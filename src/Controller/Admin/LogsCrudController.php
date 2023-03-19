6ff7d9c938f155f3fe5e4c8db4e6ee919a3bbdf8
Mikkel Ricky <rimi@aarhus.dk>
2023-03-19 11:48:52 +0100

    Added links

--- src/Controller/Admin/LogsCrudController.php
+++ src/Controller/Admin/LogsCrudController.php
@@ -10,9 +10,14 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
 class LogsCrudController extends AbstractCrudController
 {
+    public function __construct(private readonly AdminUrlGenerator $adminUrlGenerator)
+    {
+    }
+
     public static function getEntityFqcn(): string
     {
         return Logs::class;
@@ -38,10 +43,32 @@ class LogsCrudController extends AbstractCrudController
     {
         yield DateTimeField::new('createdAt');
         yield DateTimeField::new('updatedAt');
-        yield DateTimeField::new('deletedAt');
-        yield TextField::new('entity_id');
-        yield TextField::new('entity_type');
-        yield TextField::new('entity');
+        yield TextField::new('entity')
+            ->formatValue(function (?string $value, Logs $entity) {
+                if (null !== $value) {
+                    $controller = match ($entity->getEntityType()) {
+                        'software' => SoftwareCrudController::class,
+                        default => null
+                    };
+                    if (null !== $controller) {
+                        $url = $this->adminUrlGenerator
+                            ->setController($controller)
+                            ->setAction(Action::DETAIL)
+                            ->setEntityId($entity->getEntityId())
+                            ->generateUrl();
+
+                        return sprintf(
+                            '<a href="%s">%s #%s</a>',
+                            $url,
+                            $entity->getEntityType(),
+                            $entity->getEntityId()
+                        );
+                    }
+                }
+
+                return null;
+            })
+        ;
         yield TextField::new('message');
     }
 }
